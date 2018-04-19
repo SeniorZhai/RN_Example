2566085cb77a46a8ee848d0d6046f23d6c7a1197
zhaitao <370985116@qq.com>
2018-04-19 20:48:50 +0800

    clear

--- App.js
+++ App.js
@@ -6,82 +6,18 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View,
-  Alert,
-  TextInput,
-  TouchableHighlight
+  View
 } from 'react-native';
-
-// props是一个组件的参数，在父控件中设置，不可改变
-// state是一个组件mounts时设置的属性，子空间自身可以管理自己的state
-class Touch extends Component {
-  handlePress(){
-    Alert.alert("press")
-  }
-  handleLongPress(){
-    Alert.alert('longPress')
-  }
-
-  render(){
-    return (
-      <TouchableHighlight
-        onPress={this.handlePress}
-        onLongPress={this.handleLongPress}>
-        <Text>
-          Press me!
-        </Text>
-      </TouchableHighlight>
-    )
-  }
-
-}
-
+import Login from './Login'
 export default class App extends Component {
-  getDefaultProps(){
-    //
-  }
-  getInitialState(){
-    //
-  }
-
-  componentWillMount(){
-
-  }
-
-  handlerReady(str){
-    console.log(str)
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Touch/>
+        <Login />
       </View>
     );
-  }
-
-  componentDidMount(){
-
-  }
-  componentWillReceiveProps() {
-
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.id !== this.props.id;
-  }
-
-  componentWillUpdate(){
-
-  }
-
-  componentDidUpdate(){
-
-  }
-  componentWillUnmount(){
-
   }
 }
 
@@ -91,15 +27,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
---
+++ Login.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class Login extends Component {
+  render(){
+      return (
+    <View style={styles.view}></View>)
+  }
+}
+
+const styles = StyleSheet.create({
+  view:{
+    flex:1,
+    width:100,
+    height:20,
+    backgroundColor: '#FCA00F'
+  }
+})
